957a50344d8e1b6b0b420093c8a38d1a36cc8670
Rekhajambhulkar <jambhulkarrekha@gmail.com>
2020-10-27 21:10:00 +0530

    Set Player1 Position is Zero

--- SnakeAndLadderSimulator.java
+++ SnakeAndLadderSimulator.java
@@ -2,6 +2,8 @@ package com.bridgelabz.snakeandladdersimulator;
 
 public class SnakeAndLadderSimulator_1 {
 	public static void main(String args[]) {
+		//Variables
+		int Player1Position = 0;
 		// Print Welcome Message
 		System.out.println("Welcome in Snake And Ladder Simulator Program");
 	}
