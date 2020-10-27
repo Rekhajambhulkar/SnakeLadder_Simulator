89cfd499d3789bfa74b1250fb905a655ad02793d
Rekhajambhulkar <jambhulkarrekha@gmail.com>
2020-10-27 20:45:00 +0530

    Welcome In Snake and Ladder Program

---
+++ SnakeAndLadderSimulator.java
@@ -0,0 +1,8 @@
+package com.bridgelabz.snakeandladdersimulator;
+
+public class SnakeAndLadderSimulator_1 {
+	public static void main(String args[]) {
+		// Print Welcome Message
+		System.out.println("Welcome in Snake And Ladder Simulator Program");
+	}
+}
