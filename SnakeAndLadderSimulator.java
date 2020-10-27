780d7bdbc27c2198b57bc9f347108f88e2f90956
Rekhajambhulkar <jambhulkarrekha@gmail.com>
2020-10-27 22:35:58 +0530

    Play with two players

--- SnakeAndLadderSimulator.java
+++ SnakeAndLadderSimulator.java
@@ -16,10 +16,15 @@ public class SnakeAndLadderSimulator_1 {
 		System.out.println("Welcome in Snake And Ladder Simulator Program");
 		//Variables
 		int Player1Position = 0;
-		int PlayGame = 0;
+		int Player2Position = 0;
+		int PlayGame1 = 0;
+		int PlayGame2 = 0;
 		int WinPoint= 100;
 
 		while(Player1Position < 100) {
+		System.out.println();
+		System.out.println("=====PLayerOne turn=======");
+
 		int random = rollDice();
 		int option = checkOption();
 		PlayGame++;
@@ -53,13 +58,54 @@ public class SnakeAndLadderSimulator_1 {
 		}
 		System.out.println("\nPlayer1 position is:" + Player1Position);
 		System.out.println();
-		if (Player1Position < 100) {
-			continue;
-		} else {
-			// Show how many dice was played by player1
-			System.out.println("Number of time the Dice was Played by Player1: " + PlayGame + "times");
-			break;
+
+		System.out.println("=====PlayerTwo turn=====");
+			random = rollDice();
+			PlayGame2++;
+			option = checkOption();
+			// Use Switch case for option
+			switch (option) {
+			case 1:
+				System.out.print("Player2 not play");
+				break;
+			case 2:
+				Player2Position += random;
+				System.out.print("You got the Ladder" + Player2Position);
+				// In case the Player position go above 100,the player stay in the same previous
+				// position till the player gets no that adds to 100.
+				if (Player2Position > 100) {
+					Player2Position -= random;
+				}
+				break;
+			case 3:
+				Player2Position -= random;
+				System.out.print("Oops! you got the Snake" + Player2Position);
+				// In case the player position moves below 0, then the player restarts from 0
+				if (Player2Position < 0) {
+					Player2Position = 0;
+				} else {
+					Player2Position -= random;
+				}
+				break;
+			default:
+				System.out.println("Something Wrong");
+			}
+			System.out.println("\nPlayer2 position is:" + Player2Position);
+
+			// check the condition which player has won the game.
+			if (Player1Position == 100 || Player2Position == 100) {
+				if (Player1Position == 100) {
+					System.out.println();
+					System.out.println("Congratulation Player1 is won");
+					System.out.println("Number of time the Dice was Played by Player1:" + PlayGame1);
+				} else {
+					System.out.println();
+					System.out.println("Congratulation Player2 is won");
+					System.out.println("Number of time the Dice was Played by Player2:" + PlayGame2 + "times");
+
+				}
+				break;
+			}
 		}
- 	  }
-     }
+	}
 }
